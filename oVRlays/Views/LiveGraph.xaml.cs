760958cacc88a7e55e7386ff84f10017b653ec67
Nikos Tzekas <nikos.evo@hotmail.com>
2024-10-29 11:25:40 +0100

    structs optimised for speed ~250microS

--- oVRlays/Views/LiveGraph.xaml.cs
+++ oVRlays/Views/LiveGraph.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using Microsoft.VisualStudio.Utilities;
 
 namespace oVRlays.Views
 {
@@ -29,10 +30,18 @@ namespace oVRlays.Views
         //todo change that based on windows size + probably need to use now list for graphData
         private int bufferSize = 100;
         private readonly DispatcherTimer timer = new DispatcherTimer();
+        private readonly object _lock = new object();
 
-        private static double[] graphData_speed;
-        private static double[] graphData_throttle;
-        private static double[] graphData_brake;
+
+        private static CircularBuffer<double> graphData_brake;
+        private static CircularBuffer<double> graphData_throttle;
+
+        TimeSpan elapsed_buff_add;
+        TimeSpan elapsed_buff_iter;
+
+        Stopwatch stopwatch = new Stopwatch();
+
+
 
 
         public LiveGraph()
@@ -43,10 +52,11 @@ namespace oVRlays.Views
             timer.Tick += (sender,args) => RenderGraph(); // Subscribe to the Tick event
 
 
-            //initiate the graphData arrays
-            graphData_speed =  new double[bufferSize];
-            graphData_throttle = new double[bufferSize];
-            graphData_brake = new double[bufferSize];
+            //initiate the graphData buffers
+            graphData_brake = new CircularBuffer<double>(bufferSize);
+            graphData_throttle = new CircularBuffer<double>(bufferSize);
+
+
 
             // Start the timer
             timer.Start();
@@ -65,7 +75,7 @@ namespace oVRlays.Views
             double stepX = x / bufferSize;
 
 
-            Polyline throtle = new Polyline
+            Polyline throttle = new Polyline
             {
                 Stroke = Brushes.LimeGreen,
                 StrokeThickness = 2
@@ -76,45 +86,59 @@ namespace oVRlays.Views
                 StrokeThickness = 2
             };
 
-            for (int i = 0; i < bufferSize-1; i++)
-            {
 
-                //polyline.Points.Add(new Point(i * 10, new Random().NextDouble() * 300));
-                throtle.Points.Add(new Point(i*stepX, y - y* graphData_throttle[i]));
-                brake.Points.Add(new Point(i*stepX,y-y* graphData_brake[i]));
+
+            //--------------------------------------- CIRCULAR BUFFER -----------------------
+            stopwatch.Reset();
+            stopwatch.Start();
+            var brakePoints = graphData_brake.ToArray();
+            var throttlePoints = graphData_throttle.ToArray();
+
+            //todo try using foreach and for loop to see if any of em have an advantage
+            x = 0;
+            int numOfElements = brakePoints.Length;
+            for (int i = 0; i< numOfElements; i++)
+            {
+                brake.Points.Add(new Point(x * stepX, y - y * brakePoints[i]));
+                brake.Points.Add(new Point(x * stepX, y - y * throttlePoints[i]));
+
             }
-            // Add the polyline to the canvas
-            graphCanvas.Children.Add(throtle);
             graphCanvas.Children.Add(brake);
+            graphCanvas.Children.Add(throttle);
+
+            stopwatch.Stop();
+            elapsed_buff_iter = stopwatch.Elapsed;
+
+            // Add the polyline to the canvas
+            //graphCanvas.Children.Add(throtle);
 
 
         }
-
-        public void updateSpeed(double speed)
+        internal void updateSpeed(float speed)
         {
-            for (int i = 0; i < bufferSize-1; i++)
-            {
-                graphData_speed[i] = graphData_speed[i + 1];
-            }
-            graphData_speed[bufferSize-1] = speed;
+            //todo not implemented
         }
-
         internal void updateThrottle(float throttle_application)
         {
-            for (int i = 0; i < bufferSize - 1; i++)
-            {
-                graphData_throttle[i] = graphData_throttle[i + 1];
-            }
-            graphData_throttle[bufferSize - 1] = throttle_application;
+            stopwatch.Reset();
+            stopwatch.Start();
+
+            graphData_throttle.Add(throttle_application);
+
+            stopwatch.Stop();
+            elapsed_buff_add = stopwatch.Elapsed;
         }
 
         internal void updateBrake(float brake_application)
         {
-            for (int i = 0; i < bufferSize - 1; i++)
-            {
-                graphData_brake[i] = graphData_brake[i + 1];
-            }
-            graphData_brake[bufferSize - 1] = brake_application;
+
+            stopwatch.Reset();
+            stopwatch.Start();
+
+            graphData_brake.Add(brake_application);
+
+            stopwatch.Stop();
+            elapsed_buff_add = stopwatch.Elapsed;   
         }
     }
 }
---
+++ oVRlays/handlers/CircularBuffer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oVRlays.handlers
+{
+    internal class CircularBuffer<T>
+    {
+        private T[] buffer;
+        private int index = 0;
+        private int count = 0;
+        public int Capacity { get; }
+
+        public CircularBuffer(int capacity)
+        {
+            Capacity = capacity;
+            buffer = new T[capacity];
+        }
+
+        public void Add(T item)
+        {
+            buffer[index] = item;
+            index = (index + 1) % Capacity;
+
+            // Keep track of how many items have been added (up to Capacity)
+            if (count < Capacity)
+            {
+                count++;
+            }
+        }
+
+        public T[] GetData()
+        {
+            return buffer;
+        }
+    }
+}
